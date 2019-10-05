3988c47b45a38c6068a04ab67c8ef28e0ebc246c
Peter S <peter@anyline.io>
2019-10-05 17:22:14 +0200

    dragging something something..

--- game.js
+++ game.js
@@ -4,9 +4,9 @@ class DraggableObject extends Phaser.Sprite
 	constructor(x, y, sprite)
 	{
 		super(game, x, y, sprite);
-		game.physics.p2.enable(this);
+		game.physics.p2.enable(this, true);
 		this.body.clearShapes();
-		this.body.addRectangle(32, 32, 0, 0);
+		//this.body.addRectangle(32, 32, 0, 0);
 		this.animations.add('idle', [0,1,2,3], 4, true);
 		this.animations.add('walk', [4,5,6,7], 4, true);
 		this.animations.add('drag', [8], 1, true);
@@ -20,6 +20,14 @@ class DraggableObject extends Phaser.Sprite
 
 		this.isOnGround = true;
 	}
+	
+	update()
+	{
+		if (this.isOnGround)
+		{
+			this.body.rotation = 0;
+		}
+	}
 }
 
 class Cow extends DraggableObject
@@ -30,10 +38,12 @@ class Cow extends DraggableObject
 		this.state = 0; // wait
 		this.direction = 1; // right
 		this.stateTimer = 1000;
+		this.body.addRectangle(28, 20, 0, 5);		
 	}
 
 	update()
 	{
+		//super.update();
 		if (!this.isOnGround) return;
 		this.stateTimer -= game.time.elapsed;
 		switch (this.state)
@@ -68,136 +78,158 @@ class Cow extends DraggableObject
 
 class GameState extends Phaser.State
 {
-preload ()
-{
-	game.load.image('bg', 'gfx/background.png');
-	game.load.spritesheet("cow", 'gfx/cow.png', 32, 32);
-	game.load.spritesheet('gore', 'gfx/gore.png', 16, 16);
-	
-	//game.load.spritesheet('propeller', 'gfx/propeller.png', 16, 64, 4);
-	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-	//game.load.audio('music', 'sfx/theme.ogg');
-}
-
-create ()
-{
-	// game physics
-	game.physics.startSystem(Phaser.Physics.P2JS);
-	game.physics.p2.gravity.y = 200;
-  
-	this.bg = game.add.sprite(0, 0, 'bg')
-	this.livingCG = game.physics.p2.createCollisionGroup();
-	this.livingGroup = game.add.group();
-	var cow = new Cow(32, 240 - 16);
-	cow.body.setCollisionGroup(this.livingCG);
-	this.livingGroup.add(cow);
-	
-	// bg collision
-	this.bgCollision = game.add.sprite(0, 0);
-	game.physics.p2.enable(this.bgCollision);
-	this.bgCollision.body.clearShapes();
-	this.bgCollision.body.addRectangle(1024, 48, 0, 240 + 24);
-	this.bgCollision.body.static = true;
-	this.bgCollision.body.gravity = 0;
-	this.bgCG = game.physics.p2.createCollisionGroup();
-	this.bgCollision.body.setCollisionGroup(this.bgCG);
-
-	// make cow collide with background
-	cow.body.collides(this.bgCG);
-	this.bgCollision.body.collides(this.livingCG);
-
-	game.world.setBounds(0, 0, 512, 864);
-	game.camera.scale.setTo(2);
-
-	var style = { font: "14px Consolas", fill: "#ff004c", align: "center" };
-	this.debugText = game.add.text(256, 240, "debug text", style);
-	this.debugText.anchor.set(0.5);
-	this.debugText.exists = false;
-	
-	// gore emitter
-	this.goreEmitter = game.add.emitter(0, 0, 100);
-	this.goreEmitter.makeParticles('gore', [0,1,2,3,4,5,6], 300);
-	this.goreEmitter.gravity = 200;
-	this.goreEmitter.setXSpeed(-300,-100);
-
-	// drag collision
-	this.mouseBody = game.add.sprite(0, 0);
-	game.physics.p2.enable(this.mouseBody,true);
-	this.mouseBody.body.setCircle(5);
-	this.mouseBody.body.static = true;
-	this.mouseCG = game.physics.p2.createCollisionGroup();
-	this.mouseBody.body.setCollisionGroup(this.mouseCG);
-
-	
-	//define soundeffects
-	//this.wavesAudio = game.add.audio('wavesAudio');
-	
-	//play ocean waves audio
-	/*this.wavesAudio.onDecoded.add(function(){
-		if (!this.start) {
-			this.wavesAudio.play('', 0, 0, true);
-			this.wavesAudio.fadeTo(1000, 0.3);
-		}
-	}, this);*/
-
-	game.input.onDown.add(this.mouseClick, this);
-	game.input.addMoveCallback(this.mouseMove, this);
-	game.input.onUp.add(this.mouseRelease, this);
-
-	game.camera.flash('#000000');
-}
-
-mouseClick(pointer)
-{
-	console.log(pointer.position);
-	var mousePos = new Phaser.Point(pointer.x / game.camera.scale.x,
-		pointer.y / game.camera.scale.y);
-	var bodies = game.physics.p2.hitTest(mousePos, this.livingGroup.children);
-	if (bodies.length > 0)
+	preload ()
 	{
-		var body = bodies[0];
-		//this.mouseConstraint = game.physics.p2.createRevoluteConstraint(body, [0,0],this.mouseBody, [0,0]) ;
-		console.log("MOUSE CONSTRAINT");
-		this.mouseSpring = game.physics.p2.createSpring(this.mouseBody, body, 0,30,1);
-		//bodyA, bodyB, restLength, stiffness, damping, restLength, stiffness, damping, worldA, worldB, localA, localB
+		game.load.image('bg', 'gfx/background.png');
+		game.load.spritesheet("cow", 'gfx/cow.png', 32, 32);
+		game.load.spritesheet('gore', 'gfx/gore.png', 16, 16);
+		
+		//game.load.spritesheet('propeller', 'gfx/propeller.png', 16, 64, 4);
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		//game.load.audio('music', 'sfx/theme.ogg');
 	}
-}
 
-mouseMove(pointer, x, y, isDown)
-{
-	this.mouseBody.body.x = x / game.camera.scale.x;
-	this.mouseBody.body.y = y / game.camera.scale.y;
-}
+	create ()
+	{
+		// game physics
+		game.physics.startSystem(Phaser.Physics.P2JS);
+		game.physics.p2.gravity.y = 200;
+	  
+		this.bg = game.add.sprite(0, 0, 'bg')
+		this.livingCG = game.physics.p2.createCollisionGroup();
+		this.livingGroup = game.add.group();
+		var cow = new Cow(32, 240 - 16);
+		cow.body.setCollisionGroup(this.livingCG);
+		this.livingGroup.add(cow);
+		
+		// bg collision
+		this.bgCollision = game.add.sprite(0, 0);
+		game.physics.p2.enable(this.bgCollision);
+		this.bgCollision.body.clearShapes();
+		this.bgCollision.body.addRectangle(1024, 48, 0, 240 + 24);
+		this.bgCollision.body.static = true;
+		this.bgCollision.body.gravity = 0;
+		this.bgCG = game.physics.p2.createCollisionGroup();
+		this.bgCollision.body.setCollisionGroup(this.bgCG);
 
-mouseRelease()
-{
-	if (this.mouseSpring) {
-		game.physics.p2.removeSpring(this.mouseSpring);
+		// make cow collide with background
+		cow.body.collides(this.bgCG, this.cowCollides);
+		this.bgCollision.body.collides(this.livingCG);
+
+		game.world.setBounds(0, 0, 512, 864);
+		game.camera.scale.setTo(2);
+
+		var style = { font: "14px Consolas", fill: "#ff004c", align: "center" };
+		this.debugText = game.add.text(256, 240, "debug text", style);
+		this.debugText.anchor.set(0.5);
+		this.debugText.exists = false;
+		
+		// gore emitter
+		this.goreEmitter = game.add.emitter(0, 0, 100);
+		this.goreEmitter.makeParticles('gore', [0,1,2,3,4,5,6], 300);
+		this.goreEmitter.gravity = 200;
+		this.goreEmitter.setXSpeed(-300,-100);
+
+		// drag collision
+		this.mouseBody = game.add.sprite(0, 0);
+		game.physics.p2.enable(this.mouseBody,true);
+		this.mouseBody.body.setCircle(5);
+		this.mouseBody.body.static = true;
+		this.mouseCG = game.physics.p2.createCollisionGroup();
+		this.mouseBody.body.setCollisionGroup(this.mouseCG);
+
+		
+		//define soundeffects
+		//this.wavesAudio = game.add.audio('wavesAudio');
+		
+		//play ocean waves audio
+		/*this.wavesAudio.onDecoded.add(function(){
+			if (!this.start) {
+				this.wavesAudio.play('', 0, 0, true);
+				this.wavesAudio.fadeTo(1000, 0.3);
+			}
+		}, this);*/
+
+		game.input.onDown.add(this.mouseClick, this);
+		game.input.addMoveCallback(this.mouseMove, this);
+		game.input.onUp.add(this.mouseRelease, this);
+
+		game.camera.flash('#000000');
 	}
+
+	mouseClick(pointer)
+	{
+		console.log(pointer.position);
+		var mousePos = new Phaser.Point(pointer.x / game.camera.scale.x,
+			pointer.y / game.camera.scale.y);
+		var bodies = game.physics.p2.hitTest(mousePos, this.livingGroup.children);
+		if (bodies.length > 0)
+		{
+			var body = bodies[0];
+			//this.mouseConstraint = game.physics.p2.createRevoluteConstraint(body, [0,0],this.mouseBody, [0,0]) ;
+			console.log("MOUSE CONSTRAINT");
+			
+			body.parent.isOnGround = false;
+			
+			//this.mouseSpring = game.physics.p2.createSpring(this.mouseBody, body, 0, 10, 10);		
+			var localPointInBody = [0, 0];
+			var physicsPos = [game.physics.p2.pxmi(mousePos.x), game.physics.p2.pxmi(mousePos.y)];    
+			body.toLocalFrame(localPointInBody, physicsPos);
+			
+			// use a revoluteContraint to attach mouseBody to the clicked body
+			this.mouseSpring  = this.game.physics.p2.createRevoluteConstraint(this.mouseBody, [0, 0], body, [game.physics.p2.mpxi(localPointInBody[0]), game.physics.p2.mpxi(localPointInBody[1]) ]);
+			
+			//this.mouseSpring = game.physics.p2.createRevoluteConstraint(this.mouseBody, [0, 0], body, 0, 10, 10);
+			
+			//mouseConstraint = this.game.physics.p2.createRevoluteConstraint(mouseBody, [0, 0], clickedBody, [game.physics.p2.mpxi(localPointInBody[0]), game.physics.p2.mpxi(localPointInBody[1]) ]);
+			
+			//this.mouseSpring = game.physics.p2.createRotationalSpring(this.mouseBody, body, 0, 30, 1);
+			//bodyA, bodyB, restLength, stiffness, damping, restLength, stiffness, damping, worldA, worldB, localA, localB
+		}
+	}
+
+	mouseMove(pointer, x, y, isDown)
+	{
+		this.mouseBody.body.x = x / game.camera.scale.x;
+		this.mouseBody.body.y = y / game.camera.scale.y;
+		
+		
+	}
+
+	mouseRelease()
+	{
+		if (this.mouseSpring) {
+			game.physics.p2.removeConstraint(this.mouseSpring);
+		}	
+	}
+
+
+	update ()
+	{
+		// time since last frame, in seconds
+		this.deltaT = game.time.elapsed/1000;
+
+		// time since some start point, in seconds
+		this.T = game.time.now/1000;
+
+		var mouseX = game.input.activePointer.position.x / game.camera.scale.x;
+		var mouseY = game.input.activePointer.position.y / game.camera.scale.y;
+
+	}
+
+	render()
+	{
+		//game.debug.body(this.livingGroup);
+		//game.debug.body(this.bgCollision);
+	}
+
+
+	cowCollides()
+	{
+		console.log("YAY?");				
+	}
+
 }
-
-
-update ()
-{
-	// time since last frame, in seconds
-	this.deltaT = game.time.elapsed/1000;
-
-	// time since some start point, in seconds
-	this.T = game.time.now/1000;
-
-	var mouseX = game.input.activePointer.position.x / game.camera.scale.x;
-	var mouseY = game.input.activePointer.position.y / game.camera.scale.y;
-
-}
-
-render()
-{
-	//game.debug.body(this.livingGroup);
-	//game.debug.body(this.bgCollision);
-}
-
-}
-
 
 game = new Phaser.Game(
 	1024, 576,
